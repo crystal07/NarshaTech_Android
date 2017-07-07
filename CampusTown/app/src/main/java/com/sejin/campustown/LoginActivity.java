bccf67009b66ecad49030fb52eabf3d1da721933
wickedSejin <ghosejin@hanyang.ac.kr>
2017-07-07 18:05:52 +0900

    제발 되라.. 플리즈..

--- CampusTown/app/src/main/java/com/sejin/campustown/LoginActivity.java
+++ CampusTown/app/src/main/java/com/sejin/campustown/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +12,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.sejin.campustown.data.ItnlPhoneNumData;
+import com.sejin.campustown.data.IntlPhoneNumData;
 import com.sejin.campustown.data.UserData;
 
 
@@ -43,7 +42,7 @@ public class LoginActivity extends Activity {
         intl = new HashMap<String, String>();
         JSONArray jarray = null;
         try {
-            jarray = new JSONArray(ItnlPhoneNumData.intlPhoneNumData);
+            jarray = new JSONArray(IntlPhoneNumData.intlPhoneNumData);
 
             for (int i=0; i<jarray.length(); i++) {
                 JSONObject jObject = jarray.getJSONObject(i);
---
+++ CampusTown/app/src/main/java/com/sejin/campustown/data/UserData.java
@@ -0,0 +1,140 @@
+package com.sejin.campustown.data;
+
+/**
+ * Created by sejin on 2017. 7. 6..
+ */
+
+public class UserData {
+
+    private String id;
+    private String password;
+    private String email;
+    private String name;
+    private String phone;
+    // 체크해서 받는다.
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", birth='" + birth + '\'' +
+                ", index='" + index + '\'' +
+                ", university='" + university + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }private String gender;
+    private String birth;
+
+    // 생성자 ㄴㄴ
+    private String index;
+    private String university,type;
+
+    public UserData(String id, String password, String email, String name, String phone, String gender,String birth) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.gender = gender;
+        this.birth=birth;
+    }
+
+
+
+
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+
+
+
+
+
+
+}
