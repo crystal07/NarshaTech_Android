bb912499645780dc96cf00eeffa935709dd2b187
wickedSejin <ghosejin@hanyang.ac.kr>
2017-07-17 11:49:09 +0900

    소스트리 도입 전 마지막 커밋입니다.

---
+++ CampusTown/app/src/main/java/com/sejin/campustown/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.sejin.campustown.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sejin.campustown.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
